27c3b5d1f39dcf5ceff177524156dd1a8bcc022c
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2025-02-09 21:55:02 -0300

    Fix Issue

--- Profit/src/main/java/com/example/Profit/Repository/IDetallePedidoRepository.java
+++ Profit/src/main/java/com/example/Profit/Repository/IDetallePedidoRepository.java
@@ -1,8 +1,9 @@
 package com.example.Profit.Repository;
 
+import com.example.Profit.Model.DetallePedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IDetallePedidoRepository extends JpaRepository<IDetallePedidoRepository,Long> {
+public interface IDetallePedidoRepository extends JpaRepository<DetallePedido,Long> {
 }
